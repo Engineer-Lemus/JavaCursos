57f77a7c2e052070e3d29f554b8c0e5333bd086a
Dany Lemus <software.engineer.lemus@gmail.com>
2023-07-10 17:46:54 -0600

    Agregando mas informacion de cursos para java

---
+++ javaSegundoEjercicio/src/javaSegundoEjercicio/EjemploFactorial.java
@@ -0,0 +1,13 @@
+package javaSegundoEjercicio;
+
+public class EjemploFactorial {
+	public static void main(String[] args) {
+
+		int factorial = 1;
+		for (int i = 1; i < 11; i++) {
+			factorial *= i;
+			System.out.println("Factorial de " + i + " = " + factorial);
+		}
+	}
+
+}
