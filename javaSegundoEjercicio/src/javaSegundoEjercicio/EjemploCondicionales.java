280e599bbd97e31e5a0a965d1c3574e23d481369
Dany Lemus <software.engineer.lemus@gmail.com>
2023-07-10 14:51:31 -0600

    Agregando mas informacion de los cursos java

---
+++ javaSegundoEjercicio/src/javaSegundoEjercicio/EjemploCondicionales.java
@@ -0,0 +1,21 @@
+package javaSegundoEjercicio;
+
+public class EjemploCondicionales {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		int edad = 17;
+		int cantidad = 2;
+
+		if (edad >= 18) {
+			System.out.println("Usted puede entrar");
+		} else {
+			if (cantidad >= 2) {
+				System.out.println("Usted es menor de  edad pero tiene permitido entrar");
+			} else
+				System.out.println("Usted no tiene permitido " + "entrar");
+		}
+
+	}
+}
